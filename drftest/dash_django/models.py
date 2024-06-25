748fe1a037ffb666225d3360bda4e4a468ef05bd
gab <gabriel.geraldo@student.ateneo.edu>
2024-06-25 09:07:44 +0800

    added saving to drf

--- drftest/dash_django/models.py
+++ drftest/dash_django/models.py
@@ -34,14 +34,14 @@ class Character(models.Model):
         ordering = ['created']
 
     def save(self, *args, **kwargs):
-    """
-    Use the `pygments` library to create a highlighted HTML
-    representation of the code snippet.
-    """
-    lexer = get_lexer_by_name(self.language)
-    linenos = 'table' if self.linenos else False
-    options = {'title': self.title} if self.title else {}
-    formatter = HtmlFormatter(style=self.style, linenos=linenos,
-                              full=True, **options)
-    self.highlighted = highlight(self.code, lexer, formatter)
-    super().save(*args, **kwargs)
+        """
+        Use the `pygments` library to create a highlighted HTML
+        representation of the code snippet.
+        """
+        lexer = get_lexer_by_name(self.language)
+        linenos = 'table' if self.linenos else False
+        options = {'title': self.title} if self.title else {}
+        formatter = HtmlFormatter(style=self.style, linenos=linenos,
+                                full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super().save(*args, **kwargs)
